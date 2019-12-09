0478ef6e2b2daf6d706d4370ca7a8d84cf956c5d
Hernán Valdés <hfvaldesg@gmail.com>
2019-12-09 14:38:57 +0100

    Change position of camera, same distance y and z

--- CameraBody.gd
+++ CameraBody.gd
@@ -7,22 +7,23 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-#	# Move Camera
-#	if Input.is_action_pressed("ui_right"):
-#		velocity.x = SPEED
-#	elif Input.is_action_pressed("ui_left"):
-#		velocity.x = -SPEED
-#	else:
-#		velocity.x = 0
-#
-#	if Input.is_action_pressed("ui_up"):
-#		velocity.z = -SPEED
-#	elif Input.is_action_pressed("ui_down"):
-#		velocity.z = SPEED
-#	else:
-#		velocity.z = 0
-#
-#	move_and_slide(velocity)
+	# Move Camera
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = 0
+
+	if Input.is_action_pressed("ui_up"):
+		velocity.z = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.z = SPEED
+	else:
+		velocity.z = 0
+
+	move_and_slide(velocity)
+	velocity = Vector3(0, 0, 0)
 	var camera = get_node("/root/World/CameraBody/Camera")
 	
 	# Rotate Camera
@@ -35,7 +36,6 @@ func _physics_process(delta):
 			old_pos.y,
 			(-1) * old_pos.x * sin(angle) + old_pos.z * cos(angle) 
 		)
-		print(new_pos)
 		global_transform.origin = new_pos
 		camera.rotate(Vector3(0, 1, 0), 0.1)
 	elif Input.is_action_pressed("rotation") && Input.is_action_pressed("ui_left"):
@@ -47,7 +47,6 @@ func _physics_process(delta):
 			old_pos.y,
 			(-1) * old_pos.x * sin(angle) + old_pos.z * cos(angle) 
 		)
-		print(new_pos)
 		global_transform.origin = new_pos
 		camera.rotate(Vector3(0, 1, 0), -0.1)
 				
