d7aa54711505df84293bec347aee984daf1f0a05
Hernán Valdés <hfvaldesg@gmail.com>
2019-12-09 21:03:57 +0100

    WIP: Camera rotation and translation

--- CameraBody.gd
+++ CameraBody.gd
@@ -2,48 +2,33 @@ extends KinematicBody
 
 var velocity = Vector3(0, 0, 0)
 const SPEED = 10
+var angle = 0.1
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	var camera = get_node("/root/World/CameraBody/Camera")
 	var angle_camera = atan2(
 		global_transform.origin.z,
 		global_transform.origin.x
 	)
 	# Move Camera
 	if Input.is_action_pressed("ui_right"):
-		velocity = SPEED * Vector3(
-			cos(angle_camera),
-			0,
-			sin(angle_camera)
-		)
+		camera.translate(Vector3(1, 0, 0))
 	elif Input.is_action_pressed("ui_left"):
-		velocity = -SPEED * Vector3(
-			cos(angle_camera),
-			0,
-			sin(angle_camera)
-		)
-	else:
-		velocity = Vector3(0, 0, 0)
+		camera.translate(Vector3(-1, 0, 0))
 
 	if Input.is_action_pressed("ui_up"):
-		velocity.z = -SPEED
+		camera.translate(Vector3(0, 1, 1))
 	elif Input.is_action_pressed("ui_down"):
-		velocity.z = SPEED
-	else:
-		velocity.z = 0
-
-	move_and_slide(velocity)
-	velocity = Vector3(0, 0, 0)
-	var camera = get_node("/root/World/CameraBody/Camera")
+		camera.translate(Vector3(0, -1, -1))
+	
 	
 	# Rotate Camera
 	if Input.is_action_pressed("rotation") && Input.is_action_pressed("ui_right"):
-		# rotate(Vector3(0, 1, 0), 0.1)
-		print(angle_camera)
 		var old_pos = global_transform.origin
-		var angle = 0.1
+		angle = 0.1
 		var new_pos = Vector3(
 			old_pos.x * cos(angle) + old_pos.z * sin(angle),
 			old_pos.y,
@@ -52,10 +37,8 @@ func _physics_process(delta):
 		global_transform.origin = new_pos
 		camera.rotate(Vector3(0, 1, 0), 0.1)
 	elif Input.is_action_pressed("rotation") && Input.is_action_pressed("ui_left"):
-		# rotate(Vector3(0, 1, 0), -0.1)
-		print(angle_camera)
 		var old_pos = global_transform.origin
-		var angle = -0.1
+		angle = -0.1
 		var new_pos = Vector3(
 			old_pos.x * cos(angle) + old_pos.z * sin(angle),
 			old_pos.y,
