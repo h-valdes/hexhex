fee4ea720d7b367920fc18cbe645a89f9e339b66
Hernán Valdés <hfvaldesg@gmail.com>
2019-12-09 08:57:18 +0100

    WIP: Rotation of camera

--- CameraBody.gd
+++ CameraBody.gd
@@ -7,6 +7,7 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	# Move Camera
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
@@ -22,4 +23,10 @@ func _physics_process(delta):
 		velocity.z = 0
 				
 	move_and_slide(velocity)
+	
+	# Rotate Camera
+	if Input.is_action_pressed("rotation") && Input.is_action_pressed("ui_right"):
+		rotate(Vector3(0, 1, 0), 0.1)
+	elif Input.is_action_pressed("rotation") && Input.is_action_pressed("ui_left"):
+		rotate(Vector3(0, 1, 0), -0.1)
 				
