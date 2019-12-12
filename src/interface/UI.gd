24de2669d77f873636c1e4b78457b2a9f53d9a62
Hernán Valdés <hfvaldesg@gmail.com>
2019-12-12 17:39:01 +0100

    Popup window when clicking a hex with an entity

--- src/interface/UI.gd
+++ src/interface/UI.gd
@@ -10,5 +10,10 @@ func set_entity(_entity):
 	entity = _entity
 	if entity != null:
 		label_local_position.text = str(entity.get_local_position())
+		var popup = load("res://src/interface/PopInfo.tscn")
+		popup = popup.instance()
+		add_child(popup)
+		popup.get_child(0).rect_position = get_global_mouse_position()
+		popup.get_child(0).popup()
 	else:
 		label_local_position.text = "No entity"
