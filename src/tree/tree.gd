26b2a734a397c7a4c6609cd4763eea9218bb3a33
Hernán Valdés <hfvaldesg@gmail.com>
2019-12-12 21:38:28 +0100

    Add trees to the map

--- src/tree/tree.gd
+++ src/tree/tree.gd
@@ -1,15 +1,16 @@
 extends KinematicBody
 
-const knight = preload("res://entities/knight/knight.tscn")
-var knight_instance
+const tree = preload("res://entities/tree/tree.tscn")
+var tree_instance
 var local_position
 
 func _init():
-	knight_instance = knight.instance()
-	add_child(knight_instance)
+	tree_instance = tree.instance()
+	add_child(tree_instance)
 
 func get_local_position():
 	return local_position
 
 func set_local_position(_local_position):
 	local_position = _local_position
+ 
